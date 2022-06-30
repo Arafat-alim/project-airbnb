585794af569ba1c693a8ae50d1633e92ba2781b0
Arafat <arafat97alim@gmail.com>
2022-06-30 23:27:07 +0530

    hero component is completed with styled

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import photoGrid from "../images/photo-grid.png";
 
 export default function Hero() {
   return (
-    <div>
-      <h1>Hero component</h1>
-    </div>
+    <section className="hero">
+      <img src={photoGrid} alt="photo-grid" className="hero--photo" />
+      <h1 className="hero--header">Online Experiences</h1>
+      <p className="hero--text">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
   );
 }
